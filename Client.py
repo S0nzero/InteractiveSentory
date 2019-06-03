61eac79473051e1ad28f8eea58dcaacfbbb7f2a5
SDS <SDS>
2019-06-03 13:28:26 +0900

    Reading piezo

--- Client.py
+++ Client.py
@@ -1,8 +1,2 @@
 import serial as s
 ser = s.Serial('/dev/ttyACM0', 9600)
-string = "1"
-string = string.encode('ascii')
-ser.write(string)
-string = "2"
-string = string.encode('ascii')
-ser.write(string)
--- Server.py
+++ Server.py
@@ -4,9 +4,6 @@ ser = s.Serial('/dev/ttyACM0', 9600)
 while True:
     string = ser.read()
     string = string.decode('utf-8')
-    if (string =='1'):
-        print("Yay! :)")
-    else:
-        print("Nay :(")
+    print(string)
 
 

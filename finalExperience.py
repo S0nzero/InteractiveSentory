4beca569dd3d2be98c4039f8be87b9901f53fcb1
SDS <SDS>
2019-06-20 16:08:10 +0900

    Fixed an issue where a value would arrive to the program with half the data

--- finalExperience.py
+++ finalExperience.py
@@ -5,6 +5,7 @@ import csv
 import os
 import RPi.GPIO as GPIO
 
+
 def procedure():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(5, GPIO.OUT)
@@ -17,24 +18,25 @@ def procedure():
             counter += 1
         nb_sensors = input("Input the number of sensors tested: ")
     nb_sensors = int(nb_sensors)
-    counter = 0
     hdm = hitDetection.HitDetectorManager(nb_sensors, 50, 15)
     while True:
         ser = s.Serial('/dev/ttyACM0', 9600)
         result = ser.readline().decode('utf-8').split('/', 1)
-        if (len(result)==2) and (result[0].isdigit())
-        if counter != 0:
+        sensor = result[0]
+        resultofsensor = result[1][:-2]
+        print(sensor, sensor.isdigit(), result[1], resultofsensor.isdigit())
+
+        if (len(result) == 2) and (sensor.isdigit()) and (resultofsensor.isdigit()):
             if hdm.new_value(result):
                 print("Hit Detected on "+result[0]+", sending GPIO signal")
                 GPIO.output(5,1)
                 time.sleep(0.5)
                 GPIO.output(5,0)
                 time.sleep(0.5)
-                counter=0
             else:
                 print("No Hit detected")
         else:
-            counter += 1
+            print("Nope")
 
 
 procedure()

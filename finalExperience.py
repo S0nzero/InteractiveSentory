b41623167e659a5ac60139a50d0777fa1ddb57bf
Samuele <samueledasilva@gmail.com>
2019-06-20 08:50:18 +0200

    Integrated Brightsign into experience

--- finalExperience.py
+++ finalExperience.py
@@ -3,9 +3,11 @@ import serial as s
 import time
 import csv
 import os
-
+import RPi.GPIO as GPIO
 
 def procedure():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(5, GPIO.OUT)
     counter = 0
     nb_sensors = ''
     while not (nb_sensors.isdigit()):
@@ -20,10 +22,15 @@ def procedure():
     while True:
         ser = s.Serial('/dev/ttyACM0', 9600)
         result = ser.readline().decode('utf-8').split('/', 1)
+        if (len(result)==2) and (result[0].isdigit())
         if counter != 0:
             if hdm.new_value(result):
                 print("Hit Detected on "+result[0]+", sending GPIO signal")
-
+                GPIO.output(5,1)
+                time.sleep(0.5)
+                GPIO.output(5,0)
+                time.sleep(0.5)
+                counter=0
             else:
                 print("No Hit detected")
         else:
